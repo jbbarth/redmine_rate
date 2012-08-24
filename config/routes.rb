86184a4f13b6e6aef6636baec6bf998f645168cb
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-25 00:00:42 +0200

    Make the plugin boot with Redmine 2.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :rates
-  map.connect 'rate_caches', :conditions => {:method => :put}, :controller => 'rate_caches', :action => 'update'
+RedmineApp::Application.routes.draw do
+  resources :rates
+  put 'rate_caches', :to => 'rate_caches#update'
 end
